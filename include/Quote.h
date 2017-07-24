848583a2f6346a1d5b55ec5445c493168adfdaeb
Dike90 <919055994@qq.com>
2017-07-24 16:51:59 +0800

    dont know

--- include/Quote.h
+++ include/Quote.h
@@ -9,6 +9,8 @@ public:
     std::string isbn() const { return bookNo;}
     virtual double net_price(std::size_t n) const { return n * price;}
     virtual ~Quote() = default;
+    virtual Quote* clone() const & { return new Quote(*this);}
+    virtual Quote* clone() && {return new Quote(std::move(*this));}
 private:
     std::string bookNo;
 protected:
@@ -21,6 +23,8 @@ public:
     Bulk_quote(const std::string& book, double p, std::size_t qty,double disc) :
      Quote(book,p),min_qty(qty),discount(disc) {};
     double net_price(std::size_t) const override;
+    Bulk_quote* clone() const & { return new Bulk_quote(*this);}
+    Bulk_quote* clone() && {return new Bulk_quote(std::move(*this));}
 private:
     std::size_t min_qty = 0;
     double discount = 0.0;
